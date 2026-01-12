20af4c47709ea06edc7826d99c7ca424e5f74bf9
Denis <dentoc1@gmail.com>
2026-01-12 10:46:37 +0200

    Update streamlit_app.py
    Added .exposed json file behind the API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,6 @@ if ingdredients_list:
         st.success('Your Smoothie is ordered!', icon = "✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
         
